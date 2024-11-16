88983f8092611ae4e9ea52f69e44a239a1c2cbee
gaganpatil44@gmail.com <gaganpatil44@gmail.com>
2024-11-16 22:39:41 +0530

    dummy commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="text-blue-800">Hello</div>;
+  return (
+    <div className="text-red-800">
+      Hel0 adkaljdlkajdlaldjasdfsdfdslkdjllo
+      <p>lorem100</p>
+    </div>
+  );
 }
 
 export default App;
