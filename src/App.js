9282c5d4923a125171393f89991b66d7dad90060
gaganpatil44@gmail.com <gaganpatil44@gmail.com>
2024-11-16 22:41:23 +0530

    sumne

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="text-red-800">
-      Hel0 adkaljdlkajdlaldjasdf
-      <p>lorem100</p>
-    </div>
-  );
+  return <div className="text-red-800">Hel0 </div>;
 }
 
 export default App;
